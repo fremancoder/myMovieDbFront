3526b8ec6bc5d7001525dc4e794e2a470fddf684
fremancoder <fremancoder@users.noreply.github.com>
2019-10-02 09:34:07 +0200

    Update extmovielibdetail.component.ts

--- src/app/extmovielib/extmovielibdetail/extmovielibdetail.component.ts
+++ src/app/extmovielib/extmovielibdetail/extmovielibdetail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ExtMovieLibService} from "../../extmovielib.service";
 
 @Component({
   selector: 'app-extmovielibdetail',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExtmovielibdetailComponent implements OnInit {
 
-  constructor() { }
+  movieDetails;
+  cast = undefined;
+  movieId: number = undefined;
+
+  constructor(private extMovieLibService: ExtMovieLibService) {
+    this.extMovieLibService.showDetails.subscribe(
+      (movieId: number) => {
+        this.searchDetails(movieId);
+      }
+    )
+  }
 
   ngOnInit() {
   }
 
+  searchDetails(movieId: number){
+    this.cast = null;
+    this.movieId = movieId;
+    this.extMovieLibService.find(movieId).subscribe(
+         res => {this.movieDetails= res;}
+    )
+  }
+
+  showCast(){
+    if(this.movieId != null){
+      this.extMovieLibService.getCast(this.movieId).subscribe(
+        res => {this.cast= res;}
+      )
+    }
+  }
 }
