d70a9563b1fdcd38408e52e032b3910c7aa3d837
fremancoder <fremancoder@users.noreply.github.com>
2019-10-02 09:36:54 +0200

    Update extmovielib.service.ts

--- src/app/extmovielib.service.ts
+++ src/app/extmovielib.service.ts
@@ -1,4 +1,4 @@
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {HttpClient} from "@angular/common/http";
 import {EventEmitter, Injectable} from "@angular/core";
 import {catchError, retry} from "rxjs/operators";
 import {throwError} from "rxjs";
@@ -8,17 +8,31 @@ export class ExtMovieLibService {
 
   pageUpdated = new EventEmitter<number>();
 
-  constructor(private http: HttpClient) { }
+  showDetails = new EventEmitter<number>();
 
-/*  httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json'
-    })
-  }*/
+  constructor(private http: HttpClient) { }
 
   search(movieTitle: string, pageNbr: number){
     return this.http
       .get('https://api.themoviedb.org/3/search/movie?api_key=0d00a494bab46a24438adc4da1d99f0f&language=en-US&query='+movieTitle+'&page='+pageNbr+'&include_adult=false')
+      .pipe(
+        retry(1),
+        catchError(this.errorHandl)
+      )
+  }
+
+  find(movieId: number){
+    return this.http
+      .get('https://api.themoviedb.org/3/movie/'+movieId+'?api_key=0d00a494bab46a24438adc4da1d99f0f&language=en-US&append_to_response=videos')
+      .pipe(
+        retry(1),
+        catchError(this.errorHandl)
+      )
+  }
+
+  getCast(movieId: number){
+    return this.http
+      .get('https://api.themoviedb.org/3/movie/'+movieId+'/credits?api_key=0d00a494bab46a24438adc4da1d99f0f')
       .pipe(
         retry(1),
         catchError(this.errorHandl)
@@ -38,3 +52,4 @@ export class ExtMovieLibService {
   }
 
 }
+
